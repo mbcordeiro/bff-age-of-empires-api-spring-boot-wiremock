099052a8c8cb61be336ea77221d4478458e5e551
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-03-12 15:22:14 -0300

    First commit

---
+++ src/main/java/com/matheuscordeiro/bffageofempiresapi/BffAgeOfEmpiresApiApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.bffageofempiresapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgeOfEmpiresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgeOfEmpiresApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/bffageofempiresapi/BffAgeOfEmpiresApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.bffageofempiresapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgeOfEmpiresApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
