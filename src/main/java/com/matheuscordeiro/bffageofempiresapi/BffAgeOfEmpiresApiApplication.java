88c46f0959d25f2caaa633504f2cfa4706ecd39e
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-03-13 19:54:53 -0300

    Version Feign client

--- src/main/java/com/matheuscordeiro/bffageofempiresapi/BffAgeOfEmpiresApiApplication.java
+++ src/main/java/com/matheuscordeiro/bffageofempiresapi/BffAgeOfEmpiresApiApplication.java
@@ -2,8 +2,10 @@ package com.matheuscordeiro.bffageofempiresapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BffAgeOfEmpiresApiApplication {
 
 	public static void main(String[] args) {
