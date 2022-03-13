3cb0016c3ea11c2913d15170593d2dd13ba7d692
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-03-13 19:58:53 -0300

    Add annotation feign

--- src/main/java/com/matheuscordeiro/bffageofempiresapi/clients/feign/AgeOfEmpiresApi.java
+++ src/main/java/com/matheuscordeiro/bffageofempiresapi/clients/feign/AgeOfEmpiresApi.java
@@ -1,4 +1,9 @@
 package com.matheuscordeiro.bffageofempiresapi.clients.feign;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.validation.annotation.Validated;
+
+@FeignClient
+@Validated
 public interface AgeOfEmpiresApi {
 }
