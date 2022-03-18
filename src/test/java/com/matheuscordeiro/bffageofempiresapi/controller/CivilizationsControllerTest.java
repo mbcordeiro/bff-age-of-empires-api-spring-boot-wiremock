ca3673a39cae4a36316ac21385512fab391acfc4
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-03-17 23:26:21 -0300

    Setup civilization controller test

--- src/test/java/com/matheuscordeiro/bffageofempiresapi/controller/CivilizationsControllerTest.java
+++ src/test/java/com/matheuscordeiro/bffageofempiresapi/controller/CivilizationsControllerTest.java
@@ -1,4 +1,42 @@
 package com.matheuscordeiro.bffageofempiresapi.controller;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import com.matheuscordeiro.bffageofempiresapi.BffAgeOfEmpiresApiApplication;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@WireMockTest(httpPort = 8085)
+@SpringBootTest(classes = BffAgeOfEmpiresApiApplication.class)
+@TestPropertySource(locations = "classpath:application-bffControllerTest.properties")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CivilizationsControllerTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("1 - Efetuando a busca de uma civilização de acordo com o critério de pesquisa")
+    public void testFindCharacters() throws Exception {
+
+    }
+
+
+    @Test
+    @Order(2)
+    @DisplayName("2 - Civilização não encontrado")
+    public void testNotFoundCharacter() throws Exception {
+
+    }
 }
