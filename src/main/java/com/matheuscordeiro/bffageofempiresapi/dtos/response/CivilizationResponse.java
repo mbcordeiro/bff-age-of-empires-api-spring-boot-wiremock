6129d09a7417ab0cb40bef809f8b27185b00906a
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-04-13 23:53:30 -0300

    Implement test get civilizations

--- src/main/java/com/matheuscordeiro/bffageofempiresapi/dtos/response/CivilizationResponse.java
+++ src/main/java/com/matheuscordeiro/bffageofempiresapi/dtos/response/CivilizationResponse.java
@@ -2,11 +2,15 @@ package com.matheuscordeiro.bffageofempiresapi.dtos.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class CivilizationResponse {
     private Long id;
--- src/test/java/com/matheuscordeiro/bffageofempiresapi/controller/CivilizationsControllerTest.java
+++ src/test/java/com/matheuscordeiro/bffageofempiresapi/controller/CivilizationsControllerTest.java
@@ -1,7 +1,12 @@
 package com.matheuscordeiro.bffageofempiresapi.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import com.matheuscordeiro.bffageofempiresapi.BffAgeOfEmpiresApiApplication;
+import com.matheuscordeiro.bffageofempiresapi.clients.utils.ResourceUtils;
+import com.matheuscordeiro.bffageofempiresapi.dtos.response.CivilizationResponse;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,12 +14,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 @WireMockTest(httpPort = 8085)
 @SpringBootTest(classes = BffAgeOfEmpiresApiApplication.class)
-@TestPropertySource(locations = "classpath:application-bffControllerTest.properties")
+@TestPropertySource(locations = "classpath:application.yml")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CivilizationsControllerTest {
     private MockMvc mockMvc;
@@ -25,6 +38,12 @@ public class CivilizationsControllerTest {
     @Value("classpath:json/listCivilizations_OK.json")
     private Resource listCivilizationsOK;
 
+    @Value("${client.url}")
+    private String baseUrlClient;
+
+    @Value("${app.url}")
+    private String baseUrl;
+
     @BeforeEach
     public void setup() throws Exception {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
@@ -34,9 +53,25 @@ public class CivilizationsControllerTest {
     @Order(1)
     @DisplayName("1 - Efetuando a busca de uma civilização de acordo com o critério de pesquisa")
     public void testFindCharacters() throws Exception {
+        WireMock.stubFor(WireMock.get(baseUrlClient + "/civilizations")
+                .willReturn(WireMock.aResponse().withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(ResourceUtils.getContentFile(listCivilizationsOK))));
 
+        final var mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "/civilizations"))
+                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        ObjectMapper mapper = new ObjectMapper();
+        final var result = mapper.readValue(mvcResult.getResponse().getContentAsString(),
+                new TypeReference<List<CivilizationResponse>>() {});
+
+        assertFalse(result.isEmpty());
+        assertThat(result.size(), equalTo(32));
+        assertThat(result.get(0).getId(), equalTo(1L));
+        assertThat(result.get(0).getName(), equalTo("Aztecs"));
+        assertThat(result.get(0).getArmyType(), equalTo("Infantry and Monk"));
+        assertThat(result.get(0).getTeamBonus(), equalTo("Relics generate +33% gold"));
     }
-
 
     @Test
     @Order(2)
